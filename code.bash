1a2b1b2116b4fb0800e43435c882565e1f6ecfbd
moneyblx2bin <danimdaiki.tt@gmail.com>
2026-03-09 22:51:40 +0300

    Delete code.bash

--- code.bash
+++
@@ -1,3 +0,0 @@
-npx create-next-app@latest task-earning-platform --typescript --tailwind --eslint --app
-cd task-earning-platform
-npm install mongodb mongoose bcryptjs jsonwebtoken qrcode axios framer-motion lucide-react @headlessui/react @heroicons/react
